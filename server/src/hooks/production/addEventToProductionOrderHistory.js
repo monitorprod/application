f36b0474511cb83381732700d54c96f202d7b237
Luis Alberto Nunez <lan.alberto.vkrum@gmail.com>
2020-09-25 07:26:53 -0300

    Revert "removing skip short no justified"
    This reverts commit b80b032319abd6d3c0b6b62f778d58726fded185.

--- server/src/hooks/production/addEventToProductionOrderHistory.js
+++ server/src/hooks/production/addEventToProductionOrderHistory.js
@@ -189,10 +189,10 @@ module.exports = function () {
               return sum;
             }
             // skip short no justified
-            // if (EVEndD.diff(EVStartD, "minutes") < notificationTimeMIN && `${ev.at}` === `${noJustifiedActionType}`) {
-            //   if (shouldLog(ev)) console.log("> skip short no justified");
-            //   return sum;
-            // }
+            if (EVEndD.diff(EVStartD, "minutes") < notificationTimeMIN && `${ev.at}` === `${noJustifiedActionType}`) {
+              if (shouldLog(ev)) console.log("> skip short no justified");
+              return sum;
+            }
             // skip short undefined
             if (EVEndD.diff(EVStartD, "minutes") < 15 && ev.at === -1) {
               if (shouldLog(ev)) console.log("> skip short undefined");
